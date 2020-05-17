36b668f43cbff4d88e817be0555e7252113e872c
tulio <tgresende@gmail.com>
2020-05-17 19:44:40 -0300

    first commit

---
+++ src/components/TodoList/index.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+class TodoList extends Component{ 
+    render(){
+        return(
+            <div> componente
+            </div>
+        );
+    }
+}
+
+export default TodoList;
